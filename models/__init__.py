e73ba3b0c6efc33cba9d846acdd69f2cea8621d8
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-22 04:11:32 -0800

    AirBnB

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""This module instantiates an instance of the Storage will be used"""
-
+"""This module instantiates an object of class FileStorage"""
 from os import getenv
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
+if getenv('HBNB_TYPE_STORAGE') != 'db':
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
+else:
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
