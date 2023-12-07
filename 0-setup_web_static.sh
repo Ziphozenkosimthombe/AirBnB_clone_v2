bcde347e1ef707b7e709e3632f8828a4171584ea
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-12-07 22:43:11 +0200

    server ready for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
 # prepare server  for deployment of static version of AirBnB clone
 
-# run all as root
-sudo su
+sudo mkdir -p "/data"
+sudo mkdir -p "/data/web_static"
+sudo mkdir -p "/data/web_static/releases"
+sudo mkdir -p "/data/web_static/shared"
+sudo mkdir -p "/data/web_static/releases/test"
 
-mkdir "/data" && cd "/data"
-mkdir 'web_static' && cd 'web_static'
-mkdir "releases" "shared"
-mkdir "releases/test"
-
-echo "
+echo '
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>AirBnB clone</title>
 	</head>
-    <body style=\"margin:0px;padding:0px;\">
-		<header style=\"background-color:#FF0000;height:70px;width:100%;\">
+    <body style="margin:0px;padding:0px;">
+		<header style="background-color:#FF0000;height:70px;width:100%;">
 		</header>
 
-		<footer style=\"position:fixed;bottom:0;background-color:#00FF00;height:60px;width:100%;text-align:center;line-height:30px;\">
+		<footer style="position:fixed;bottom:0;background-color:#00FF00;height:60px;width:100%;text-align:center;line-height:30px;">
 			<p>Holberton School</p>
 		</footer>
 	</body>
 </html>
-" > 'releases/test/index.html'
+' | sudo tee "/data/web_static/releases/test/index.html"
 
-cd ~
 # Define paths
 # link_path="/data/web_static/current"
 # target_path="/data/web_static/releases/test"
@@ -34,15 +31,15 @@ cd ~
 # Check if the symbolic link already exists
 if [ -L "$link_path" ]; then
     # If it exists, delete it
-    rm "$link_path"
+    sudo rm "$link_path"
 fi
 
 # Create the symbolic link
-ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-echo "
+sudo echo "
 server {
     listen 80;
     server_name _;
@@ -57,6 +54,6 @@ server {
         index index.html;
     }
 }
-" > '/etc/nginx/sites-available/default'
+" | sudo tee '/etc/nginx/sites-available/default'
 
-service nginx restart
+sudo service nginx restart
