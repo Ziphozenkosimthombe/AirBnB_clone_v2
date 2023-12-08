7db2952ace0bfbce1f486220ead0a4d914b2f8c7
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-08 01:31:59 -0800

    solutions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,63 +1,31 @@
 #!/usr/bin/env bash
-# prepare server  for deployment of static version of AirBnB clone
+# sets up my web servers for the deployment of web_static
 
+# Install Nginx if not already installed
 sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+sudo apt-get install -y nginx
 
-sudo mkdir -p "/data"
-sudo mkdir -p "/data/web_static"
-sudo mkdir -p "/data/web_static/releases"
-sudo mkdir -p "/data/web_static/shared"
-sudo mkdir -p "/data/web_static/releases/test"
+# Create folders if not already exists
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-echo '
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>AirBnB clone</title>
-	</head>
-    <body style="margin:0px;padding:0px;">
-		<header style="background-color:#FF0000;height:70px;width:100%;">
-		</header>
+# Create a fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 
-		<footer style="position:fixed;bottom:0;background-color:#00FF00;height:60px;width:100%;text-align:center;line-height:30px;">
-			<p>Holberton School</p>
-		</footer>
-	</body>
-</html>
-' | sudo tee "/data/web_static/releases/test/index.html"
+# Create & recreate a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Define paths
-link_path="/data/web_static/current"
-# target_path="/data/web_static/releases/test"
-
-# Check if the symbolic link already exists
-if [ -L "$link_path" ]; then
-    # If it exists, delete it
-    sudo rm "$link_path"
-fi
-
-# Create the symbolic link
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
+# Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo echo "
-server {
-    listen 80;
-    server_name _;
+# update Nginx config to serve /data/web_static/current at /hbnb_static/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
-    location / {
-        add_header X-Served-By 226796-web-01;
-        add_header Content-Type text/plain;
-        return 200 'Hello World!\n';
-    }
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-        index index.html;
-    }
-}
-" | sudo tee '/etc/nginx/sites-available/default'
-
+# Restart Nginx to apply changes
 sudo service nginx restart
