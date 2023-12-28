968c6ac1186b378c43da23c6b3788529fe592092
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-28 07:08:08 -0800

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""flask web application"""
+"""Start Flask web application"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Routing to root, strict_slashes ensure
+    the URL works
+    """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=500)
+    app.run(host='0.0.0.0', port=5000)
