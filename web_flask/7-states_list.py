8c4726dda7ca2de29b6d2b7a8ac6d9238c3aad7d
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-01-02 12:16:19 -0800

    task 07-state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-'''A flask app displaying states in a web page'''
+"""List of states on HTML states_list"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def close_session(exception=None):
-    '''Remove the current SQLAlchemy Session after each request'''
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    '''Display all states in the database'''
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    """display a HTML page: (inside the tag BODY)"""
+    sortedlist = sorted(storage.all(
+        State).values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", sorted_states_list=sortedlist)
+
+
+@app.teardown_appcontext
+def terminate(exc):
+    """close the storage"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    """start the server"""
+    app.run(host='0.0.0.0', port=5000)
